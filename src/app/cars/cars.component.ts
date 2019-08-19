2261b014d656efd78e9e007bfa79a9d724c84cef
Yura <47417811+krainskyi@users.noreply.github.com>
2019-08-19 16:53:26 +0300

    mycars demo 19.08 work

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -11,32 +11,38 @@ import { TranslationWidth } from '@angular/common';
 export class CarsComponent implements OnInit {
 
   constructor(private carService: CarService) { }
-
+  
   ngOnInit() {
   }
   public cars: any = [];
-  public car = { brand: '', price: '', year: '', used: false };
-
+  public car = { brand: '', price: '', year: '', used: true };
+  
   world: string = 'car';
   translation: string = 'машина';
   status: boolean = false;
 
- 
-  getWorldStatus() {
+  getWorldStatus(){
     return this.status ? 'car work' : 'car on service';
-
+    
   }
-  public sortA(cars) {
-    cars.sort((a, b) => {
-      return a.price - b.price
+
+  public sortA(cars){
+    cars.sort((a,b)=>{
+      return a.price-b.price
     })
-
+    
+}
+ 
+  carDelete(index){
+    this.carService.deleteCar(index)
   }
-  carPush(newCar) {
+  
+  carPush(newCar){
     this.carService.addCar(newCar)
     this.cars = this.carService.cars;
-  }
+  } 
 
+ 
 
 }
 
